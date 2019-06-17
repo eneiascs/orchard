bccf1ddaee653167289063dc1d189745cad4bd0d
Eneias Cordeiro da Silva <eneiascs@gmail.com>
2019-06-17 10:51:02 -0300

    Add missing annotations

--- src/main/java/com/eneiascs/orchard/model/UserRole.java
+++ src/main/java/com/eneiascs/orchard/model/UserRole.java
@@ -1,6 +1,7 @@
 package com.eneiascs.orchard.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
+@Entity
 public class UserRole {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
