4ab2c0577ea3763f4c916cf8a8bbcc737bd0a6c3
Eneias Cordeiro da Silva <eneiascs@gmail.com>
2019-06-17 09:59:09 -0300

    First commit

---
+++ src/main/java/com/eneiascs/orchard/OrchardApplication.java
@@ -0,0 +1,13 @@
+package com.eneiascs.orchard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchardApplication.class, args);
+	}
+
+}
