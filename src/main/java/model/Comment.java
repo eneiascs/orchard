3aac61b37cfb9d64cf6621d8ab75eb166081daf6
Eneias Cordeiro da Silva <eneiascs@gmail.com>
2019-06-17 10:44:31 -0300

    Add model

---
+++ src/main/java/model/Comment.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false, nullable = false)
+	private Integer id;
+	private String username;
+	
+	@Column(columnDefinition = "text")
+	private String content;
+	private Date postedDate;
+	
+	public Comment() {
+		
+	}
+	public Comment(Integer id, String username, String content, Date postedDate) {
+		this.id = id;
+		this.username = username;
+		this.content = content;
+		this.postedDate = postedDate;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getPostedDate() {
+		return postedDate;
+	}
+	public void setPostedDate(Date postedDate) {
+		this.postedDate = postedDate;
+	}
+	
+	
+}
